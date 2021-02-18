20b01d61ab1966e57ef6acb405d2d130ea9f89d2
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-02-18 16:23:53 +0530

    1.1.hydro start

---
+++ src/typeDefs/section_1_1/section_1_1_hydro.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+import datetime as dt
+
+
+class ISection_1_1_hydro(TypedDict):
+    tot_month_hydro_gen_mu: float
+    tot_last_year_hydro_gen_mu: float
+    tot_last_year_hydro_gen_mu_perc_inc: float
+    avg_month_hydro_gen_mu: float
+    max_month_hydro_gen_mu: float
+    max_month_hydro_gen_mu_date: str
