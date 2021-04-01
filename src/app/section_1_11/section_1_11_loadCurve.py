ccc806cb4a27477d48599bc3f35bb061da4f53f4
Nitin Sharma <nitin.s@wrldc.local>
2021-04-01 12:35:55 +0530

    load Curve colors rendering

--- src/app/section_1_11/section_1_11_loadCurve.py
+++ src/app/section_1_11/section_1_11_loadCurve.py
@@ -49,9 +49,9 @@ def fetchSection1_11_LoadCurve(appDbConnStr: str, startDt: dt.datetime, endDt: d
 
     # ax.stackplot(x, y , labels=['Wind+Solar' ,'Net Load', 'WR Demand'],baseline='sym')
 
-    ax.fill_between(x,y[0],color=clr[0],alpha=0.5,label='Wind+Solar')
-    ax.fill_between(x,y[1],color=clr[1],alpha=0.5,label='Net Load')
-    ax.fill_between(x,y[2],color=clr[2],alpha=0.5,label='WR Demand')
+    ax.fill_between(x,y[0],color='red',alpha=0.5,label='Wind+Solar')
+    ax.fill_between(x,y[1],color='blue',alpha=0.5,label='Net Load')
+    ax.fill_between(x,y[2],color='black',alpha=0.5,label='WR Demand')
 
     ax.yaxis.grid(True)
     ax.legend(bbox_to_anchor=(0.5,-0.2,0.0, 0.0), loc='center',
@@ -63,7 +63,6 @@ def fetchSection1_11_LoadCurve(appDbConnStr: str, startDt: dt.datetime, endDt: d
     fig.subplots_adjust(bottom=0.25, top=0.8)
 
     fig.savefig('assets/section_1_11_netLoadCurve.png')
-    plt.show()
     plt.close()
 
     secData: dict = {}
