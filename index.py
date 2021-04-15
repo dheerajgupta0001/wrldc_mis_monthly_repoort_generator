d51164c7059a43c855a52b2c9210d1e2acedee74
Nitin Sharma <nitin.s@wrldc.local>
2021-04-15 14:39:59 +0530

    cahnges

--- index.py
+++ index.py
@@ -19,6 +19,6 @@ tmplPath: str = "templates/monthly_rep_template.docx"
 
 # create weekly report
 mnthlyRprtGntr = MonthlyReportGenerator(appDbConStr,outageDbConStr)
-monthDt = dt.datetime(2021,3,1)
+monthDt = dt.datetime(2021,2,1)
 mnthlyRprtGntr.generateMonthlyReport(monthDt, tmplPath, dumpFolder)
 print('Report generation Done')
